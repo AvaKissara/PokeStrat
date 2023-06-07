41e9453e6d79c76cc069f2938a70967bbb5ec603
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2023-06-08 00:54:18 +0200

    MVersion

---
+++ Modeles/MVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStrat.Modeles
+{
+    public class MVersion
+    {
+        public int idVersion { get; }
+        public string nomVersion { get; set; }
+        public int emlplacement { get; set; }
+
+        public MVersion(int IdVersion, string NomVersion, int Emplacement) 
+        {
+            this.idVersion = IdVersion;
+            this.nomVersion = NomVersion;
+            this.emlplacement = Emplacement;
+        }
+    }
+}
