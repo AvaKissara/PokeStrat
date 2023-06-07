b5f18853e7a40bf56ea6ed328e0a9fc7474a726b
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2023-06-08 01:13:53 +0200

    modeles suite

---
+++ Modeles/MCapacite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStrat.Modeles
+{
+    public class MCapacite
+    {
+        public int idCapacite { get; }
+        public string nomCapacite { get; set; }
+        public MType typeCapacite { get; set; }
+        public MCategorie catCapacite { get; set; }
+
+        public MCapacite(int IdCapacite, string NomCapacite, MType TypeCapacite, MCategorie CatCapacite)
+        {
+            this.idCapacite = IdCapacite;
+            this.nomCapacite = NomCapacite;
+            this.typeCapacite = TypeCapacite;
+            this.catCapacite = CatCapacite;
+        }
+    }
+}
---
+++ Modeles/MCategorie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStrat.Modeles
+{
+    public class MCategorie
+    {
+        public int idCategorie { get; }
+        public string nomCategorie { get; set; }
+
+        public MCategorie(int IdCategorie, string NomCategorie)
+        {
+            this.idCategorie = IdCategorie;
+            this.nomCategorie = NomCategorie;
+        }
+    }
+}
