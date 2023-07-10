8237124d2c1a837ea34aecb86e9f57f160c20e3c
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2023-07-10 15:06:12 +0200

    modele

--- Modeles/MMatch.cs
+++ Modeles/MMatch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace PokeStat.Modeles
 {
-    internal class MMatch
+    public class MMatch
     {
+        public int idMatch { get; }
+        public int dureeMatch { get; set; }
+        List<MEquipe> equipes { get; set;}
     }
 }
